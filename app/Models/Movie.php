29446fcf5659d7097e430052b080cb712b221105
mateuszlasek <mateuszlasek100@gmail.com>
2026-02-22 21:11:43 +0100

    Add Movie model and migration

---
+++ app/Models/Movie.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Translatable\HasTranslations;
+
+class Movie extends Model
+{
+    use HasTranslations;
+
+    public array $translatable = ['title', 'overview'];
+
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'overview',
+        'original_title',
+        'poster_path',
+        'backdrop_path',
+        'release_date',
+        'vote_average',
+        'vote_count',
+        'popularity',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'release_date' => 'date',
+        ];
+    }
+
+    public function genres(): BelongsToMany
+    {
+        return $this->belongsToMany(Genre::class);
+    }
+}
