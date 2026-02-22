dab51050ae28ef52f5dc845ee863bc1de756254d
mateuszlasek <mateuszlasek100@gmail.com>
2026-02-22 21:55:07 +0100

    ImportMoviesJob

---
+++ app/Jobs/ImportMoviesJob.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Services\TmdbService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class ImportMoviesJob implements ShouldQueue
+{
+    use Queueable;
+
+    private const TOTAL = 50;
+
+    private const PER_PAGE = 20;
+
+    public function handle(TmdbService $tmdb): void
+    {
+        $imported = 0;
+        $page = 1;
+
+        while ($imported < self::TOTAL) {
+            $movies = $tmdb->getPopularMovies($page++);
+
+            foreach ($movies as $data) {
+                if ($imported >= self::TOTAL) {
+                    break;
+                }
+
+                $localeEn = $data['_locales']['en'];
+
+                $movie = Movie::updateOrCreate(
+                    ['tmdb_id' => $data['tmdb_id']],
+                    [
+                        'title' => [
+                            'en' => $data['_locales']['en']['title'] ?? '',
+                            'pl' => $data['_locales']['pl']['title'] ?? '',
+                            'de' => $data['_locales']['de']['title'] ?? '',
+                        ],
+                        'overview' => [
+                            'en' => $data['_locales']['en']['overview'] ?? '',
+                            'pl' => $data['_locales']['pl']['overview'] ?? '',
+                            'de' => $data['_locales']['de']['overview'] ?? '',
+                        ],
+                        'original_title' => $localeEn['original_title'] ?? '',
+                        'poster_path' => $localeEn['poster_path'] ?? null,
+                        'backdrop_path' => $localeEn['backdrop_path'] ?? null,
+                        'release_date' => $localeEn['release_date'] ?: null,
+                        'vote_average' => $localeEn['vote_average'] ?? 0,
+                        'vote_count' => $localeEn['vote_count'] ?? 0,
+                        'popularity' => $localeEn['popularity'] ?? 0,
+                    ]
+                );
+
+                $genreIds = Genre::whereIn('tmdb_id', $localeEn['genre_ids'] ?? [])
+                    ->pluck('id');
+
+                $movie->genres()->sync($genreIds);
+
+                $imported++;
+            }
+        }
+    }
+}
